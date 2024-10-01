e0c3b8fca805f575207d6d7f87c1095c97dd5701
Amaan Mohammad Khalander <syedkhalander66@gmail.com>
2024-10-01 20:11:18 +0530

    chore: Training and Testing Cat & Dog's IMG also understanding Batch Size where ( total batches 250 = 8000 image / 36 batch size )

--- algorithms/7 deep-learning/convolutional_neural_network.py
+++ algorithms/7 deep-learning/convolutional_neural_network.py
@@ -16,12 +16,11 @@ training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
-
 # Preprocessing the Test set
 test_datagen = ImageDataGenerator(rescale = 1./255)
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
-                                            batch_size = 32,
+                                            batch_size = 32, 
                                             class_mode = 'binary')
 
 # Part 2 - Building the CNN
